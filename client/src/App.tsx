63bb68d0a24cea16b5acdc0a8939762918887908
clong desktop <chip_long@yahoo.com>
2022-03-24 10:23:50 -0600

    added CSS

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 import FileUpload from "./app/components/FileUpload";
 
 function App() {
   return (
-    <div>
-      <h2>React/Node js file upload example</h2>
-      <FileUpload />
+    <div className="myContainer">
+      <div className="myCard myBackground">
+        <h2 className="p-2">React/Node js file upload example</h2>
+        <FileUpload />
+      </div>
     </div>
   );
 }
--- client/src/app/components/FileUpload.tsx
+++ client/src/app/components/FileUpload.tsx
@@ -1,32 +1,64 @@
-import React, { ChangeEvent, useState } from "react";
+import { ChangeEvent, useState } from "react";
+import ImagePreview from "./ImagePreview";
+// import "./FileUpload.css";
 
 function FileUpload() {
   const [file, setFile] = useState<File>();
   const [fileName, setFileName] = useState("");
+  const [statusMsg, setStatusMsg] = useState("");
   const [errMsg, setErrMsg] = useState("");
 
-  const saveFile = (e: ChangeEvent<HTMLInputElement>) => {
-    if (!e.target.files) {
+  const imageHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    let selectedFile: File | null = e.target.files ? e.target.files[0] : null;
+
+    if (!selectedFile) {
       setErrMsg("Choose a file");
       return;
     }
 
     setErrMsg("");
-    setFile(e.target.files[0]);
-    setFileName(e.target.files[0].name);
+    setFile(selectedFile);
+    setFileName(selectedFile.name);
   };
 
   const uploadFile = () => {
-    console.log("do something");
+    if (file) {
+      const formData = new FormData();
+      formData.append("file", file);
+      formData.append("fileName", fileName);
+
+      const options: RequestInit = {
+        method: "POST",
+        headers: {
+          Accept: "multipart/form-data",
+        },
+        body: formData,
+      };
+      fetch("http://localhost:3000/upload", options)
+        .then((res) => res.json())
+        .then((res) => setStatusMsg(res.msg))
+        .catch((err) => {
+          setErrMsg("There was an error in upload");
+          console.log("upload catch error:");
+          console.log(err);
+        });
+    }
   };
 
   return (
-    <div>
-      <div className="App">
-        <input type={"file"} onChange={saveFile} />
+    <div className="myCard">
+      <div>
+        <input
+          type="file"
+          name="image"
+          accept="image/*"
+          multiple={false}
+          onChange={imageHandler}
+        />
         <button onClick={uploadFile}>Upload</button>
       </div>
-      <p>{errMsg}</p>
+      {statusMsg && <p className="status">{statusMsg}</p>}
+      {errMsg && <p className="status error">{errMsg}</p>}
     </div>
   );
 }
