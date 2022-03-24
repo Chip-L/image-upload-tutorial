e02b8e6f6ad1fb1ef969d07ffaa95b8b62949e5d
clong desktop <chip_long@yahoo.com>
2022-03-24 10:54:10 -0600

    added multer configuration

--- server/server.js
+++ server/server.js
@@ -1,7 +1,19 @@
 const express = require("express");
+const multer = require("multer");
 
 const app = express();
 const PORT = 3001;
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, "./"),
+  filename: (req, file, cb) => {
+    const ext = file.mimetype.split("/")[1];
+    cb(null, `uploads/${file.originalname}-${Date.now()}.${ext}`);
+  },
+});
+const upload = multer({
+  storage,
+});
 
 app.listen(PORT, (err) => {
   if (err) console.log(err);
